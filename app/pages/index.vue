0a6486f3f3d2843f9b411dbb424f761ed047a68f
Steve Fallet <steve.fallet@divtec.ch>
2025-11-19 15:21:08 +0100

    refactor: merge filter and software grid sections to remove spacing

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -599,13 +599,11 @@ useSeoMeta({
           </UButton>
         </div>
       </div>
-    </UPageSection>
 
-    <!-- Software Grid -->
-    <UPageSection>
+      <!-- Software Grid -->
       <div
         v-if="filteredSoftwareList.length > 0"
-        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
+        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6"
       >
         <SoftwareCard
           v-for="software in filteredSoftwareList"
